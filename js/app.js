0d26f001a976ccb67bd94b617629c2dffc5f1961
“emilykiss” <emilylanekiss@gmail.com>
2022-05-06 14:48:22 -0700

    Basic page structure

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log("is this workin 👀");
+
+// var count = 4;
+// function anim() {
+//   if (count > 0) {
+//     console.log(count);
+//     count--;
+//     setTimeout(anim, 700);
+//   } else {
+//     alert("GO!!");
+//   }
+// }
+// anim();
