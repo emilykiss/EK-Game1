98d4b64b36525962affea500f2286a60a4a8d477
“emilykiss” <emilylanekiss@gmail.com>
2022-05-11 12:11:15 -0700

    pngs added

--- js/app.js
+++ js/app.js
@@ -5,19 +5,18 @@ const updatedScore = document.querySelector("#score")
 const collide = document.querySelector("#status1")
 document.addEventListener("keydown", movementHandler)
 
-//Setting the dimensions of the canvas
-const ctx = canvas.getContext("2d");
 
-//Had to set the dimensions to hard numbers- screen was acting wonky 
+//Setting the dimensions of the canvas
+const ctx = canvas.getContext("2d")
+
+//Had to set the dimensions to hard numbers- screen was acting wonky
 canvas.height = 444
 canvas.width = 880
 
 //This score will change after running the gameLoop function
-let score = 100
-
+let score = 100;
 
 setInterval(gameLoop, 60)
-
 
 //setting an object that contains the player's info
 class Object {
@@ -31,14 +30,14 @@ class Object {
   }
 
   render() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height)
   }
 }
 
 //This function is used to generate a random number for spawning objs
-function generateRandom(){
-  let randomNumber = Math.floor(Math.random()*canvas.width - 30)
+function generateRandom() {
+  let randomNumber = Math.floor(Math.random() * canvas.width - 30)
   return randomNumber
 }
 
@@ -48,47 +47,45 @@ const donut = new Object(generateRandom(), 0, "aqua", 32, 48)
 const veggie = new Object(generateRandom(), 0, "lightgreen", 32, 48)
 
 function drawBox(x, y, w, h, color) {
-  ctx.fillStyle = color;
+  ctx.fillStyle = color
   ctx.fillRect(x, y, w, h)
 }
 
 //My gravity function- rate at which items are falling
-const speed = 8;
+const speed = 8
 const drop = setInterval(function () {
   donut.y += speed
   veggie.y += speed
 })
 
-
-//Arrays to push the foods into - we will need this for the gameLoop to operate 
+//Arrays to push the foods into - we will need this for the gameLoop to operate
 let donuts = []
 let veggies = []
 
-const donutSpawn = setInterval(function(){
-  donuts.push( new Object(generateRandom(), 0, "aqua", 32, 48))
+const donutSpawn = setInterval(function () {
+  donuts.push(new Object(generateRandom(), 0, "aqua", 32, 48))
 }, 3000)
 
 const veggieSpawn = setInterval(function () {
-  veggies.push(new Object(generateRandom(), 0, "lightgreen", 32, 48));
-}, 3500);
-
+  veggies.push(new Object(generateRandom(), 0, "lightgreen", 32, 48))
+}, 3500)
 
 function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  shopper.render()
-  for(let i = 0; i < donuts.length; i++){
-    donuts[i].render()
-    donuts[i].y += speed
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  shopper.render();
+  for (let i = 0; i < donuts.length; i++) {
+    donuts[i].render();
+    donuts[i].y += speed;
     if (
       shopper.x < donuts[i].x + donuts[i].width &&
       shopper.x + donuts[i].width > donuts[i].x &&
       shopper.y < donuts[i].y + donuts[i].height &&
       shopper.y + shopper.height > donuts[i].y
     ) {
-        collide.innerText = "🍩 Keep it up! 🍩";
-        donuts.splice(i, 1)
-        updatedScore.innerText = score 
-        score = score + 50
+      collide.innerText = "🍩 Keep it up! 🍩";
+      donuts.splice(i, 1);
+      updatedScore.innerText = score;
+      score = score + 50;
     }
   }
   for (let i = 0; i < veggies.length; i++) {
@@ -112,7 +109,7 @@ function gameLoop() {
   }
 }
 // }
-//Controls the arrow keys and speed of the player 
+//Controls the arrow keys and speed of the player
 function movementHandler(e) {
   const speed = 60;
   console.log(e.key);
@@ -128,7 +125,6 @@ function movementHandler(e) {
 
 //Start new game- reload the page
 document.querySelector("#restart").addEventListener("click", function () {
-  console.log("hey");
-  location.reload();
-}) 
-
+  console.log("hey")
+  location.reload()
+});
